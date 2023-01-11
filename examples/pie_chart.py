9ab7255caaed759932e608d1af6ee7fe177eec44
greufe-kr <114253393+greufe-kr@users.noreply.github.com>
2023-01-11 15:24:13 -0500

    updated with necessary parameter

--- examples/pie_chart.py
+++ examples/pie_chart.py
@@ -4,6 +4,6 @@ from colby import cb
 exhibit = cb.Exhibit([3,2], normal_font = "texgyreheros-regular.otf", bold_font = "texgyreheros-bold.otf", 
                      italic_font = "texgyreheros-italic.otf", bold_italic_font = "texgyreheros-bolditalic.otf")
 exhibit.add_panel_nonts(1, 0, 0)
-exhibit.plot_panel_cs_pie(1, [.25, .5, .75, 1], ["red", "green", "blue", "orange"], label_list = ["Red", "Green", "Blue", "Orange"], radius = .7)
+exhibit.plot_panel_cs_pie(1, [.25, .5, .75, 1], ["red", "green", "blue", "orange"], label_list = ["Red", "Green", "Blue", "Orange"],start_angle=0, radius = .7)
 exhibit.add_panel_text(1, .05, .9, "Title", scale = "fixed")
 exhibit.save_exhibit("pie_chart.pdf")
